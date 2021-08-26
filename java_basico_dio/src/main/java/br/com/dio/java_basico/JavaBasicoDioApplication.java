6505484f8ec73b5ca3e37f9a5da1aa26ae01e5b5
Geverson Souza <geversonjosedesouza@gmail.com>
2021-08-26 07:18:43 -0300

    Curso de Java Básico

---
+++ java_basico_dio/src/main/java/br/com/dio/java_basico/JavaBasicoDioApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.java_basico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasicoDioApplication.class, args);
+	}
+
+}

18978f731822ea52f502ec2cd928021651659e86
Geverson <geversonjosedesouza@gmail.com>
2022-05-27 23:07:53 -0300

    Processamento paralelo e acompanhando as threads.

--- src/main/java/br/com/sfdio/SfsdioApplication.java
+++ src/main/java/br/com/sfdio/SfsdioApplication.java
@@ -3,21 +3,18 @@ package br.com.sfdio;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.sfdio.java_avancado.ExecutorExemplo;
-import br.com.sfdio.java_avancado.ServicoNotificacao;
-import br.com.sfdio.java_avancado.ServicoNotificacao.TipoCliente;
 
 @SpringBootApplication
 public class SfsdioApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SfsdioApplication.class, args);
 		//Java Avançado
 		//String[] nomes = {"João", "Paula", "Maria", "Geverson","Geverson"};
 		//IterandoFuncao.imprimindoNomesFiltradosAntigo(nomes);
 		//IterandoFuncao.imprimindoNomesFiltradosNovo(nomes);
 		//new CriandoPrimeiraThread(false);
 		new ExecutorExemplo();
-		new ServicoNotificacao(TipoCliente.PF);
+		//new ServicoNotificacao(TipoCliente.PF);
 	}
 
 }
--- src/main/java/br/com/sfdio/java_avancado/ExecutorExemplo.java
+++ src/main/java/br/com/sfdio/java_avancado/ExecutorExemplo.java
@@ -3,8 +3,13 @@ package br.com.sfdio.java_avancado;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class ExecutorExemplo {
 
@@ -12,7 +17,32 @@ public class ExecutorExemplo {
 
 	public ExecutorExemplo() {
 		Casa casa = new Casa(new Quarto());
-		casa.obterAFazerDaCasa().forEach(atividade -> EXECUTOR_SERVICE.execute(() -> atividade.realizar()));
+		List<? extends Future<String>> futures = new CopyOnWriteArrayList<>(
+				casa.obterAFazerDaCasa().stream().map(atividade -> EXECUTOR_SERVICE.submit(() -> {
+					return atividade.realizar();
+				})).collect(Collectors.toList()));
+		while (true) {
+			int numeroDeAtividadesNaoFinalizada = 0;
+			for (Future<?> future : futures) {
+				if (future.isDone())
+					try {
+						System.out.println("Concluído..." + future.get());
+						futures.remove(future);
+					} catch (InterruptedException | ExecutionException e) {
+						e.printStackTrace();
+					}
+				else
+					numeroDeAtividadesNaoFinalizada++;
+			}
+			if (futures.stream().allMatch(Future::isDone))
+				break;
+			try {
+				Thread.sleep(1000);
+				System.out.println("Número de ativiades não finalizadas = " + numeroDeAtividadesNaoFinalizada);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		EXECUTOR_SERVICE.shutdown();
 	}
 
@@ -38,29 +68,63 @@ public class ExecutorExemplo {
 
 	public class Quarto extends Comodo {
 
+		private static final String ARRUMAR_PENTEADEIRA = "arrumarPenteadeira...";
+		private static final String ARRUMAR_CAMA = "arrumarCama...";
+		private static final String ARRUMAR_GRARDA_ROUPA = "arrumarGrardaRoupa...";
+		private static final String VARRENDO = "Varrendo...";
+
 		@Override
 		List<Atividade> obterAFazerDoComodo() {
 			return Arrays.asList(this::varrer, this::arrumarCama, this::arrumarGrardaRoupa, this::arrumarPenteadeira);
 		}
 
-		private void varrer() {
-			System.out.println("Varrendo...");
+		private String varrer() {
+			try {
+				Thread.sleep(gerarSegundosAleatorioDe100a5000());
+				System.out.println(VARRENDO);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return VARRENDO;
 		}
 
-		private void arrumarGrardaRoupa() {
-			System.out.println("arrumarGrardaRoupa...");
+		private String arrumarGrardaRoupa() {
+			try {
+				Thread.sleep(gerarSegundosAleatorioDe100a5000());
+				System.out.println(ARRUMAR_GRARDA_ROUPA);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return ARRUMAR_GRARDA_ROUPA;
 		}
 
-		private void arrumarCama() {
-			System.out.println("arrumarCama...");
+		private String arrumarCama() {
+			try {
+				Thread.sleep(gerarSegundosAleatorioDe100a5000());
+				System.out.println(ARRUMAR_CAMA);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return ARRUMAR_CAMA;
 		}
-		
-		private void arrumarPenteadeira() {
-			System.out.println("arrumarPenteadeira...");
+
+		private String arrumarPenteadeira() {
+			try {
+				Thread.sleep(gerarSegundosAleatorioDe100a5000());
+				System.out.println(ARRUMAR_PENTEADEIRA);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return ARRUMAR_PENTEADEIRA;
+		}
+
+		private static int gerarSegundosAleatorioDe100a5000() {
+			Random random = new Random();
+			return random.nextInt((5000 - 100) + 1) + 100;
 		}
 	}
 
 	public interface Atividade {
-		void realizar();
+		String realizar();
 	}
 }
